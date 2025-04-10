233df58670b17a12c917b36b0a736ddec3b35d51
YuriyNik156 <yuriy.nik156@gmail.com>
2025-04-10 14:53:03 +0300

    add end_game condition

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Импортирование модуля tkinter
 import tkinter as tk
 from tkinter import messagebox
 
+# Создание главного окна для игры крестики-нолики
 window = tk.Tk()
-window.title("Крестики-нолики")
-window.geometry("380x500")
+window.title("Крестики-нолики. Игра до 3-х побед.")
+window.geometry("460x540")
 window.configure(bg="#f0f0f0")
 
+# Инициализация переменных
 current_player = "X"
 player_choice = None
 buttons = []
@@ -14,16 +17,27 @@ score_x = 0
 score_o = 0
 score_label = None
 
-
+# Создание подсчета очков каждого игрока и условие победы до 3-х очков
 def update_score(winner):
     global score_x, score_o
     if winner == "X":
         score_x += 1
     elif winner == "0":
         score_o += 1
+
     score_label.config(text=f"Счёт — X: {score_x} | 0: {score_o}")
 
+    if score_x == 3 or score_o == 3:
+        end_game(winner)
 
+# Создание окончания игры после 3-х побед и возможность начать новую игру
+def end_game(winner):
+    status_label.config(text=f"Игрок {winner} одержал 3 победы и выиграл игру!")
+    disable_all_buttons()
+    reset_button.pack_forget()
+    new_game_button.pack(pady=10)
+
+# Условия, при которых игроки получают очки
 def check_winner():
     for i in range(3):
         if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
@@ -42,7 +56,7 @@ def check_winner():
 
     return False
 
-
+# Условия ничьи
 def is_draw():
     for row in buttons:
         for btn in row:
@@ -50,7 +64,7 @@ def is_draw():
                 return False
     return True
 
-
+# Основная логика игры. Заполнение полей Х и 0
 def on_click(row, col):
     global current_player
 
@@ -73,18 +87,18 @@ def on_click(row, col):
     current_player = "0" if current_player == "X" else "X"
     status_label.config(text=f"Ход игрока: {current_player}")
 
-
+# Подсвечиваем зеленым цветом кнопки, по которым прошла выигрышная линия
 def highlight_buttons(coords):
     for r, c in coords:
         buttons[r][c].config(bg="#7bed9f")
 
-
+# Отключаем кнопки после окончания раунда
 def disable_all_buttons():
     for row in buttons:
         for btn in row:
             btn.config(state="disabled")
 
-
+# Добавление возможности сброса текущей игры
 def reset_game():
     global current_player
     current_player = player_choice
@@ -93,7 +107,17 @@ def reset_game():
             btn.config(text="", state="normal", bg="white")
     status_label.config(text=f"Ход игрока: {current_player}")
 
+# Сброс текущего счета у обоих игроков
+def new_game():
+    global score_x, score_o
+    score_x = 0
+    score_o = 0
+    score_label.config(text=f"Счёт — X: {score_x} | 0: {score_o}")
+    new_game_button.pack_forget()
+    reset_button.pack(pady=15)
+    reset_game()
 
+# Запуск игры после выбора символа Х или 0 игроком
 def start_game(choice):
     global current_player, player_choice
     player_choice = choice
@@ -141,8 +165,13 @@ for i in range(3):
         row.append(btn)
     buttons.append(row)
 
-reset_button = tk.Button(game_frame, text="Сбросить игру", font=("Arial", 14), bg="#74b9ff", fg="white",
+reset_button = tk.Button(game_frame, text="Сбросить раунд", font=("Arial", 14), bg="#74b9ff", fg="white",
                          activebackground="#0984e3", command=reset_game)
 reset_button.pack(pady=15)
+
+new_game_button = tk.Button(game_frame, text="Новая игра", font=("Arial", 14), bg="#00b894", fg="white",
+                            activebackground="#00cec9", command=new_game)
+
+new_game_button.pack_forget()
 
 window.mainloop()
